39592bbed6f324e73d70c24bf101d4a9aaf72565
Isaiah Gonzalez <79392395+IsaiahGonz@users.noreply.github.com>
2025-10-22 15:37:07 -0500

    Update layout.js
    Google Analytics wasn't detecting ShareSkippy since it's a Next.js website. It was searching for the page but fails to find the analytics code since the page takes some time to become interactive.

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
 				{/* Google Analytics */}
 				<Script
 					src="https://www.googletagmanager.com/gtag/js?id=G-TGM53SZZX1"
-					strategy="afterInteractive"
+					strategy="beforeInteractive"
 				/>
 				<Script id="google-analytics" strategy="afterInteractive">
 					{`
