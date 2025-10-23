3e5a633cedb4b7594e1c218e1dcc2923bb84c3ef
Kaia <kcolban@gmail.com>
2025-10-22 17:46:44 -0700

    Trigger deployment: Add deployment comment to layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export const viewport = {
 
 // This adds default SEO tags to all pages in our app.
 // You can override them in each page passing params to getSOTags() function.
+// Updated for deployment trigger
 export const metadata = getSEOTags();
 
 export default function RootLayout({ children }) {
