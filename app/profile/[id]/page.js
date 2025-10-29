1489293aef8aed4750a4ca3712c6d6ce738445d9
tgenz1213 <timothy.genz@yahoo.com>
2025-10-29 09:00:58 -0500

    feat(profile): Use Next/Image for public profile and dog photos

--- app/profile/[id]/page.js
+++ app/profile/[id]/page.js
@@ -3,7 +3,6 @@ import { useCallback, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import { useEffect, useState, useCallback } from 'react';
 import { createClient } from '@/libs/supabase/client';
 import { formatLocation } from '@/libs/utils';
 import UserReviews from '@/components/UserReviews';
