cea5d48bf577d746db397a57c416d1eacfd92000
tgenz1213 <timothy.genz@yahoo.com>
2025-11-01 18:46:40 -0500

    chore(deps): configure jest to use babel-jest for all JS/TS/JSX files

--- jest.config.ts
+++ jest.config.ts
@@ -175,8 +175,7 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
