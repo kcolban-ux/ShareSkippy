7e5ffa09c72824f25582acf858a94925d652d3e8
Timothy Genz <timothy.genz@yahoo.com>
2025-11-19 14:02:01 -0600

    feat: Add testPathIgnorePatterns to jest.config.ts for improved test isolation

--- jest.config.ts
+++ jest.config.ts
@@ -95,6 +95,7 @@ const config: Config = {
   transform: {
     "^.+\\.(t|j)sx?$": "babel-jest",
   },
+  testPathIgnorePatterns: ["\\node_modules\\", "<rootDir>/tests/e2e/"],
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
