5a24c81ac78b1be3381c3bca5d3eec7a86607834
Timothy Genz <34835862+Tgenz1213@users.noreply.github.com>
2026-01-21 11:29:27 -0600

    Remove unused components and API utilities to streamline the codebase

--- components/ui/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Button } from './Button';
-export { default as Testimonial, TestimonialGrid } from './Testimonial';
-export { default as Icon } from './Icon';
-export { default as Section } from './Section';
-export { default as PhotoUpload } from './PhotoUpload';
-export { default as UserRatingsEnhanced } from './UserRatingsEnhanced';
